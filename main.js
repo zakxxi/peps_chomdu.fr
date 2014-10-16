27ac7c2133af424bcee2296fcb0ca04006ba0b00
Adrien Revel <adrien@abrege.net>
2014-10-16 11:25:24 +0200

    Base theme for chomdu.fr: homepage html and basic styles

---
+++ main.js
@@ -0,0 +1,14 @@
+/* CUSTOM JQUERY CODE */
+
+$(function() {
+
+/* FlexSlider options */
+	$('.flexslider').flexslider({
+    	animation: "slide", // fade
+		controlNav: false,
+		directionNav: false,
+		slideshowSpeed: 4000,
+		animationSpeed: 500,  
+    });
+
+});
